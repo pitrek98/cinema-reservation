f004389279cb9f3d014b13581379c530a6d36bf2
Pszymiec <piotrekszymiec@gmail.com>
2026-06-09 14:57:50 +0200

    Set retry policy

--- app/db.py
+++ app/db.py
@@ -1,13 +1,18 @@
-from cassandra.cluster import Cluster
-from cassandra.policies import RoundRobinPolicy
+from cassandra.cluster import EXEC_PROFILE_DEFAULT, Cluster, ExecutionProfile
+from cassandra.policies import ConsistencyLevel, DowngradingConsistencyRetryPolicy, RoundRobinPolicy
 
 def get_session():
+    profile = ExecutionProfile(
+        retry_policy=DowngradingConsistencyRetryPolicy(),
+        request_timeout=20,
+    )
+
     cluster = Cluster(
         contact_points=[
             "127.0.0.1"
         ],
         port=9042,
-        load_balancing_policy=RoundRobinPolicy()
+        execution_profiles={EXEC_PROFILE_DEFAULT: profile}
     )
 
     session = cluster.connect("cinema")
